3a743729ffdade3f69c6ea163c52fdfb5733efaa
Azizul Bappy <azizulbappy6@gmail.com>
2021-07-03 18:23:51 +0600

    working on post page bottom

--- src/components/post/Main.js
+++ src/components/post/Main.js
@@ -3,13 +3,15 @@ import Image from "next/image"
 
 // --- components
 import TitleSection from './TitleSection'
-import Content from './Content'
+import Content from './Content/Content'
+import Bottom from "./Bottom/Bottom"
 
 function Main() {
     return (
         <div className="container px-4 md:px-16 my-8 mx-auto min-h-screen">
             <TitleSection />
             <Content />
+            <Bottom />
         </div>
     )
 }
